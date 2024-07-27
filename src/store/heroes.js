8b475cb45e87fa473ff4bf733c456a3e610a60fd
Vinicius Back Ferreira <103704757+negovinicius@users.noreply.github.com>
2024-07-27 14:11:00 -0300

    feat: :sparkles: Get configurate on store

--- src/store/heroes.js
+++ src/store/heroes.js
@@ -3,7 +3,21 @@ import api from '../services/rest';
 
 export const useHeroesStore = defineStore('heroes', {
     state: () => ({
-        
+        heroes: [],
+        isLoading: false, 
     }),
-    actions: {}
+    actions: {
+        async takeHeroes() {
+            try {
+                this.isLoading = true;
+                const response = await api.heroes.getHeroes();
+                
+                return response;
+            } catch (error) {
+                console.log('Error on GetHeroes', error)
+            } finally {
+                this.isLoading = false;
+            }
+        }
+    }
 });
